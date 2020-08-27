0c58155575b5f635275dd74fcc755ab4e5dfbe23
Daniel Gould <dmgould310@gmail.com>
2020-08-27 12:37:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,6 +30,13 @@ class Student
   end 
   
   def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.album)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   
   
   def self.create 
